4dda7f4b0b76223a3827aad3a4fbe294826063f2
HotJuly <949588380@qq.com>
2021-11-19 15:00:31 +0800

    17.小程序生命周期

--- 1.小程序/02.代码/wechat-init/pages/log/log.js
+++ 1.小程序/02.代码/wechat-init/pages/log/log.js
@@ -12,21 +12,21 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+        // console.log('onLoad')
     },
 
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
     onReady: function () {
-
+        // console.log('onReady')
     },
 
     /**
      * 生命周期函数--监听页面显示
      */
     onShow: function () {
-
+        // console.log('onShow')
     },
 
     /**
