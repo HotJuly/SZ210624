f4a207982d6032900227f4a1b0b9cb613ae54022
HotJuly <949588380@qq.com>
2021-11-20 11:57:39 +0800

    25.小程序数据请求

--- 1.小程序/02.代码/硅谷云音乐/pages/index/index.js
+++ 1.小程序/02.代码/硅谷云音乐/pages/index/index.js
@@ -5,14 +5,36 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        // 用于存储轮播图数据
+        banners:[]
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+        /*
+            1.在哪发
+                onLoad
+            2.怎么发
+                API:wx.request(Object object)
+            3.往哪发
+                看接口文档url:/banner
+            注意:小程序中,全局对象不是window,全局对象是wx
+        */
+    //    console.log(wx)
+        wx.request({
+            url:"http://localhost:3000/banner",
+            data:{
+                type:2
+            },
+            success:(data)=>{
+                // console.log(data)
+                this.setData({
+                    banners:data.data.banners
+                })
+            }
+        })
     },
 
     /**
