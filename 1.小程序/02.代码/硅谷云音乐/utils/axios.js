d8e800b072f16fd5a195190bd9b9d621d41f822d
HotJuly <949588380@qq.com>
2021-11-22 09:27:15 +0800

    33.小程序自定义组件拆分

--- 1.小程序/02.代码/硅谷云音乐/utils/axios.js
+++ 1.小程序/02.代码/硅谷云音乐/utils/axios.js
@@ -7,19 +7,26 @@
         1.保留重复出现的代码
         2.将每次不同的内容提取为形参
         3.谁调用谁传入
-*/
 
+    封装组件
+        1.保留重复出现的结构和少量js
+        2.将每次不同的内容提取为props
+        3.谁使用谁传入
+*/
+import config from './config';
 export default function(url,data={},method="GET"){
 
     // 发送请求是同步的,返回响应是异步的
     return new Promise((resolve,reject)=>{
         wx.request({
-            url:"http://localhost:3000" + url,
+            url:config.mpHost + url,
             data,
             method,
             success:(res)=>{
                 // console.log(res)
                 // result= res;
+
+                // res是响应报文对象,res.data是响应体
                 resolve(res.data);
             }
         })
