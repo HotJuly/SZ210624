dd52acf6f53989a1f70879894463af7b22d65211
HotJuly <949588380@qq.com>
2021-12-04 08:35:32 +0800

    后台管理系统文档上传

--- 1.小程序/02.代码/硅谷云音乐/app.js
+++ 1.小程序/02.代码/硅谷云音乐/app.js
@@ -3,5 +3,15 @@ App({
         msg:"我是全局的初始化数据",
         audioId:null,
         playState:false
+    },
+    onLaunch(){
+        function PageInit(page){
+            Object.keys(page).forEach((key)=>{
+                console.log('key',key)
+            })
+            return Page1(page);
+        }
+        let Page1 = Page
+        Page = PageInit
     }
 })
