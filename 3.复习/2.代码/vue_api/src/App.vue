39357960d53c6b2f4a444a2038626dbdbfcb2c4e
HotJuly <949588380@qq.com>
2021-12-11 11:49:19 +0800

    35.Vue-API(剩余内容)

--- 3.复习/2.代码/vue_api/src/App.vue
+++ 3.复习/2.代码/vue_api/src/App.vue
@@ -2,21 +2,41 @@
   <div id="app">
     <!-- <HelloWorld :value="msg" @input="data=>msg=data" /> -->
     <!-- <HelloWorld ref="hello" :msg.sync="msg" /> -->
-    <HelloWorld ref="hello" :msg="msg" @update:msg="data=>msg=data" />
+    <!-- <HelloWorld ref="hello" :msg="msg" @update:msg="data=>msg=data" /> -->
     <!-- <input type="text" v-model="msg"> -->
     <!-- <input type="text" :value="msg" @input="event=>msg=event.target.value"> -->
-    <h1 ref="h1" @click="handleClick">{{msg}}</h1>
-    <h2 v-once>{{user.name}}</h2>
+    <!-- <h1 ref="h1" @click="handleClick">{{msg}}</h1> -->
+    <!-- <h2 v-once>{{user.name}}</h2> -->
+
+    <!-- <A v-if="flag"></A>
+    <B v-if="!flag"></B> -->
+    <!-- <component :is="showComponent"></component>
+    <button @click="handleClick">切换</button> -->
+    <A>
+      <template>
+        <h1>我是默认插槽1</h1>
+      </template>
+      <template v-slot:header>
+        <h1>我是具名插槽header</h1>
+      </template>
+      <template #footer="{title}">
+        <h1>我是作用域插槽footer,{{title}}</h1>
+      </template>
+    </A>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import A from './components/A.vue'
+// import B from './components/B.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  //   // HelloWorld
+    A,
+  //   B
   },
   data(){
     return{
@@ -24,7 +44,9 @@ export default {
       phone:"18888888",
       user:{
         name:"123"
-      }
+      },
+      flag:true,
+      showComponent:A
     }
   },
   mounted(){
@@ -39,8 +61,11 @@ export default {
       // this.$forceUpdate();
       // this.$destroy();
       // console.log(123)
-      this.user.name=222222;
-      console.log(123)
+      // this.user.name=222222;
+      // console.log(123)
+
+      // this.flag=!this.flag
+      this.showComponent= B;
     }
   }
 }
